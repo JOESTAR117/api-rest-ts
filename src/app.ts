174bc4e2c987dadcd9a6ba9e8abdff7dbe9e07a2
JOESTAR117 <juniorrios1231@gmail.com>
2023-05-15 10:42:07 -0300

    feat: prisma init

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
 
 const app = express()
 
 app.use(express.json())
+
+app.get('/', (req: Request, res: Response) => {
+	return res.json('Hello World')
+})
 
 export { app }
